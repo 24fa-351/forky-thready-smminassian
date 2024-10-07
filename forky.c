b21500ca2ef7f75a31ada320f68e62aa6c1da78c
smminassian <sebastienminassian203@gmail.com>
2024-10-06 22:36:17 -0700

    I am still working on this project. I am focusing on finishing collatz and min heap right now. Sorry for it taking so long

---
+++ forky.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+void pattern1(int things);
+void pattern2(int things);
+
+int main(int __argc, char *__argv[])
+{
+    int things = 0;
+    int pattern = 0;
+
+    if (__argc == 3)
+    {
+        things = atoi(__argv[1]);
+        pattern = atoi(__argv[2]);
+
+        if(pattern == 1){
+            pattern1(things);
+        }
+        else if(pattern == 2){
+            pattern2(things);
+        }
+
+    }
+}
