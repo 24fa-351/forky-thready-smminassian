b488198384f5876b4a2955ae796b375aacaaf128
smminassian <sebastienminassian203@gmail.com>
2024-10-20 16:04:07 -0700

    added a lot of things however, it still needs some work. Im definitly getting something right in my results but i dont know why its not changing freqently. I need to add a dupliactae catcher for rand maybe that will help

--- forky.c
+++ forky.c
@@ -4,30 +4,69 @@
 #include <unistd.h>
 #include <sys/types.h>
 
-int pattern1(int things);
-void pattern2(int things);
+void pattern1(int things, FILE *fptr);
+// void pattern2(int things);
+void childPattern1(int children, pid_t pid, FILE *fptr, int things);
 
 int main(int __argc, char *__argv[])
 {
     int things = 0;
     int pattern = 0;
-    pid_t pid = fork();
+
+    FILE *fptr;
+    fptr = fopen("results.txt", "w");
 
     if (__argc == 3)
     {
         things = atoi(__argv[1]);
         pattern = atoi(__argv[2]);
 
-        if(pattern == 1){
-            pattern1(things);
+        if (pattern == 1)
+        {
+            pattern1(things, fptr);
         }
-        else if(pattern == 2){
-            pattern2(things);
-        }
-
+        // else if (pattern == 2)
+        // {
+        //     pattern2(things);
+        // }
     }
 }
 
-int pattern1(int things){
+void pattern1(int things, FILE *fptr)
+{
+    int ix;
+    // int duplicateProcess[things];
     
+
+    // int dupProcessCount = 0;
+
+    for (ix = 1; ix <= things; ix++)
+    {
+        int children = rand() % (5);
+        // if(dupProcessCount != 0){
+        //     if(duplicateProcess[dupProcessCount] = randProcess){
+
+        //     }
+        fprintf(fptr, "Pattern 1: creating %d processes\n", things);
+        pid_t pid = fork();
+        for (int dx = 0; dx <= children; dx++)
+        {
+            fprintf(fptr, "Parent created child %d (pid %d)\n", dx, getpid());
+        }
+        childPattern1(children, pid, fptr, things);
+    }
+}
+
+// each process is independent from each other. So this means that children should be random and process should be random
+
+void childPattern1(int children, pid_t pid, FILE *fptr, int things)
+{
+    int Min = 1;
+    int Max = 8;
+    int sleepyTime = (rand() % (Max-Min + Min) + Min);
+    int randProcess = (rand() % (things - Min + Min) + Min);
+
+    fprintf(fptr, "Process %d (pid %d) created: will sleep for %d seconds\n", randProcess, getpid(), sleepyTime);
+    sleep(sleepyTime);
+    fprintf(fptr, "Process %d (pid %d): exiting\n", randProcess, getpid());
 }
