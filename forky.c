fd2f8aefaa5e92c27aa86959e1ee14e7bd4cb4fe
smminassian <sebastienminassian203@gmail.com>
2024-11-21 14:47:17 -0800

    extremely close to getting right answer its just my completed print statement is malfunctioning

--- forky.c
+++ forky.c
@@ -9,7 +9,7 @@
 
 void pattern1(int things, FILE *fptr);
 // void pattern2(int things);
-void childPattern1(int children, pid_t pid, FILE *fptr, int things);
+void childPattern1(pid_t pid, FILE *fptr, int things);
 
 int main(int argc, char *argv[])
 {
@@ -41,45 +41,40 @@ void pattern1(int things, FILE *fptr)
     int pids[things];
     fprintf(fptr, "Pattern 1: creating %d processes\n", things);
     
+    for(int ix = 1; ix < things ; ix++){
     
-    for (ix = 0; ix <= things; ix++)
-    {
-    	pid_t pid = fork();
-    	fprintf(fptr, "Parent Created child %d,with pid %d\n", ix, getpid());
-    	pids[ix] = pid;
-    }
-    for(int jx = 0; jx <= things ; jx++){
-    
-      	fflush(fptr);
+    	fflush(fptr);
+      	pid_t pid = fork();
     	
-    	if(pids[jx] == 0){
-    	
-        	int children = rand() % (5);
-        	childPattern1(children, pids[jx], fptr, things);
+    	if(pid == 0){
+    		fprintf(fptr, "Parent created child %d with pid %d \n", ix, getpid());
+        	childPattern1(pid, fptr, things);
         	exit(0);
         }
-        else if(pids[jx] > 0){
-       		int status;
-		wait(&status);
+        else if(pid > 0){
+    		int status;
+    		waitpid(pids[ix], &status, 0);
 		fprintf(fptr, "Child %d with pid: %d completed\n", ix, getpid());
-     	}	
+		}
+     		
         else{
      		perror("Fork Failed");
      		exit(1);
-     	}   
+     	}  
+     	} 
      } 
-}
+
 
 
 // each process is independent from each other. So this means that children should be random and process should be random
 
-void childPattern1(int children, pid_t pid, FILE *fptr, int things)
+void childPattern1(pid_t pid, FILE *fptr, int things)
 {
     int Min = 1;
     int Max = 8;
     int sleepyTime = (rand() % (Max-Min + Min) + Min);
     int randProcess = (rand() % (things - Min + Min) + Min);
-
+    
     fprintf(fptr, "Process %d (pid %d) created: will sleep for %d seconds\n", randProcess, getpid(), sleepyTime);
     sleep(sleepyTime);
     fprintf(fptr, "Process %d (pid %d): exiting\n", randProcess, getpid());
